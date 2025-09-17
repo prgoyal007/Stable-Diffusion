eb124a75040ab17bffe03d3531f654926c1e1fd6
Prabhav Goyal <prabhav.goyal@gmail.com>
2025-09-17 03:16:26 -0700

    Update ddpm.py

--- sd/ddpm.py
+++ sd/ddpm.py
@@ -22,6 +22,45 @@ class DDPMSampler:
         timesteps = (np.arange(0, num_inference_steps) * step_ratio).round()[::-1].copy().astype(np.int64)
         self.timesteps = torch.from_numpy(timesteps)
 
+    def _get_previous_timestep(self, timestep: int) -> int:
+        prev_t = timestep - (self.num_inference_steps // self.num_inference_steps)
+        return prev_t
+    
+    def _get_variance(self, timestep: int) -> torch.Tensor:
+        prev_t = self._get_previous_timestep(timestep)
+
+        alpha_prod_t = self.alpha_cumprod[timestep]
+        alpha_prod_t_prev = self.alpha_cumprod[prev_t] if prev_t >= 0 else self.one
+        current_beta_t = 1 - alpha_prod_t / alpha_prod_t_prev
+
+        # Computed using formula (7) of the DDPM paper
+        variance = (1 - alpha_prod_t_prev) / (1 - alpha_prod_t) * current_beta_t
+        variance = torch.clamp(variance, min=1e-20)
+
+        return variance
+
+    def step(self, timestep: int, latents: torch.Tensor, model_output: torch.Tensor):
+        t = timestep
+        prev_t = self._get_previous_timestep(t)
+
+        alpha_prod_t = self.alpha_cumprod[timestep]
+        alpha_prod_t_prev = self.alpha_cumprod[prev_t] if prev_t >= 0 else self.one
+        beta_prod_t = 1 - alpha_prod_t
+        beta_prod_t_prev = 1 - alpha_prod_t_prev
+        current_alpha_t = alpha_prod_t / alpha_prod_t_prev
+        current_beta_t = 1 - current_alpha_t
+
+        # Compute the predicted original sample using formula (15) of the DDPM paper
+        pred_original_sample = (latents - beta_prod_t ** 0.5 * model_output) / alpha_prod_t ** 0.5
+
+        # Compute the coefficients for pred_original_sample and current sample x_t
+        pred_original_sample_coeff = (alpha_prod_t_prev ** 0.5 * current_beta_t) / beta_prod_t
+        current_sample_coeff = current_alpha_t ** 0.5 * beta_prod_t_prev / beta_prod_t
+
+        # Compute the predicted previous sample mean
+        pred_prev_sample = pred_original_sample_coeff * pred_original_sample + current_sample_coeff * latents
+
+
     def add_noise(self, original_samples: torch.FloatTensor, timesteps: torch.IntTensor) -> torch.FloatTensor:
         alpha_cumprod = self.alpha_cumprod.to(device=original_samples.device, dtype=original_samples.dtype)
         timesteps = timesteps.to(original_samples.device)
